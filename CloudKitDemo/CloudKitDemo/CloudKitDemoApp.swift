c60c4940d84cfc4f7f6b79c9c045a2a812a484b7
ByoungilYoun <jimmy7525@naver.com>
2026-01-04 01:01:59 +0900

    Project Init

---
+++ CloudKitDemo/CloudKitDemo/CloudKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudKitDemoApp.swift
+//  CloudKitDemo
+//
+//  Created by 윤병일 on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct CloudKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
